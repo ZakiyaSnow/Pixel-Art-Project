1365622c3a66b3eec5536ea703d45fe2e5d5bf10
Zakiya Moore-Snowden <zakiya.moore226@gmail.com>
2022-04-26 21:48:00 -0400

    Create designs.js

---
+++ designs.js
@@ -0,0 +1,32 @@
+function makeGrid(){
+    const rows = document.getElementById('inputHeight').value;
+    const cols = document.getElementById('inputWidth').value;
+
+    // Select element to create grid
+    const tbl = document.getElementById('pixelCanvas');
+
+    // Clear table before creating grid
+    tbl.innerHTML="";
+
+    for (i = 0; i < rows; i++) {
+        let myRow =document.createElement('tr');
+        myRow = tbl.insertRow(i);
+    for (j = 0; j < cols; j++) {
+        let myCell = document.createElement('td');
+        myCell = myRow.insertCell(j);
+
+    // Add color to cell
+            myCell.addEventListener('click', () => {
+                myCell.style.backgroundColor = document.getElementById('colorPicker').value;
+            });
+        }
+    }
+}
+
+// Create form for submission
+const form = document.getElementById('sizePicker');
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    makeGrid();
+});
